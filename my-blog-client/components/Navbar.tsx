33c77dd3a15f75ee91ac7657a27062e3ec6636df
Prince Khunt <princekhuntdatastructure@gmail.com>
2022-11-14 12:29:48 +0530

    Create navbar

---
+++ my-blog-client/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="flex item-center justify-between py-4 boxshedow-fullwidth">
+            <Link href="/">
+                <div className="flex items-center cursor-pointer">
+                    <span className="text-3xl text-primary font-caveatbrush">
+                        My Blog
+                    </span>
+                </div>
+            </Link>
+            <ul className="flex items-center gap-7">
+                <li className="text-gray-600">
+                    <a href="#">Products</a>
+                </li>
+                <li className="text-gray-600">
+                    <a href="#">pricing</a>
+                </li>
+                <li className="text-gray-600">
+                    <a href="#">Docs</a>
+                </li>
+                <li className="text-gray-600">
+                    <a href="#">Company</a>
+                </li>
+            </ul>
+            <ul className="flex items-center gap-7">
+                <li className="text-primary">
+                    <a href="#" className="hover:text-gray-400">
+                        Log in
+                    </a>
+                </li>
+                <li className="text-gray-600">
+                    <a
+                        href="#"
+                        className="rounded-sm text-primary transition-all">
+                        Sign up
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ my-blog-client/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#53BD95',
+        primarydark: '#2c785c'
+      },
+      boxShadow: {
+        fullwidth: '0 0 0 100vmax #53bd9515'
+      },
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      caveatbrush: 'Caveat Brush'
+    }
+  },
+  plugins: [],
+}
